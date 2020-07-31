6be2debe3e57b814116b402c945d280409e54ba9
arnavgupta03 <arnavgupt03@gmail.com>
2020-07-31 14:35:42 -0400

    Fix index html and js for disabling buttons

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var ZetaDisabled = true;
-var KappaDisabled = true;
-var PsiDisabled = true;
+function disableButtons() {
+    var ZetaDisabled = true;
+    var KappaDisabled = true;
+    var PsiDisabled = true;
 
-document.getElementById("zeta").disabled = ZetaDisabled;
-document.getElementById("kappa").disabled = KappaDisabled;
-document.getElementById("psi").disabled = PsiDisabled;
+    document.getElementById("zeta").disabled = ZetaDisabled;
+    document.getElementById("kappa").disabled = KappaDisabled;
+    document.getElementById("psi").disabled = PsiDisabled;
+}
