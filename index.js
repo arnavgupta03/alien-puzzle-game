b3dfbbde8d6c32e91555ac8684344dea06ea554f
arnavgupta03 <arnavgupt03@gmail.com>
2020-07-31 14:12:44 -0400

    Add js file and change index, puzzle comment for epsilon

---
+++ index.js
@@ -0,0 +1,7 @@
+var ZetaActive = false;
+var KappaActive = false;
+var PsiActive = false;
+
+document.getElementById("zeta").enabled = ZetaActive;
+document.getElementById("kappa").enabled = KappaActive;
+document.getElementById("psi").enabled = PsiActive;
