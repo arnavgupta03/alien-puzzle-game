e4a791e954ed3af2ab58d49bafba1bd3133cc1a2
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-01 07:11:03 -0400

    Fixed bugs with zeta button disable in index js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function disableButtons() {
+    var epsilonDone = "false";
     var ZetaDisabled = true;
     var KappaDisabled = true;
     var PsiDisabled = true;
-    var epsilonDone = localStorage.getItem("epsilonDone");
+    if (localStorage.getItem("epsilonDone") != null){
+        epsilonDone = localStorage.getItem("epsilonDone");
+    }
 
+    document.getElementById("kappa").disabled = KappaDisabled;
+    document.getElementById("psi").disabled = PsiDisabled;
     if (epsilonDone.localeCompare("true") != 0){
         document.getElementById("zeta").disabled = ZetaDisabled;
     }
-    document.getElementById("kappa").disabled = KappaDisabled;
-    document.getElementById("psi").disabled = PsiDisabled;
 }
 function onLoad() {
     disableButtons();
