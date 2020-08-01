9f4a2260315d3b538a138862e5fb702c1f63c8e9
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-01 17:10:27 -0400

    Added zeta chatbot for first question

--- epsilon.js
+++ epsilon.js
@@ -1,6 +1,6 @@
 function writeHint() {
     var hint = document.createElement("div");
-    hint.innerHTML = "Hint: Alpha=>Unary, Beta=>Binary, Gamma=>Tertiary, Delta=>Quaternary";
+    hint.innerHTML = "<p style='color:white, font-family:'sans-serif''>Hint: Alpha=>Unary, Beta=>Binary, Gamma=>Tertiary, Delta=>Quaternary</p>";
     hint.style.width = "100%";
     hint.style.position = "absolute";
     document.body.appendChild(hint);
