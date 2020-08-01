7d555fd97eee114871b1bc8ef917b813c708f395
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-01 08:54:04 -0400

    Fix zeta hint spacing

--- zeta.js
+++ zeta.js
@@ -1,6 +1,6 @@
 function writeHints() {
     var hints = document.createElement("div");
-    hints.innerHTML = "Hint 1: Think about the man stabbed 23 times on March 15. (Et tu, Brute?)  Hint 2: The number you require again relates to what letter Zeta is in the Greek alphabet.";
+    hints.innerHTML = "Hint 1: Think about the man stabbed 23 times on March 15. (Et tu, Brute?) <br> Hint 2: The number you require again relates to what letter Zeta is in the Greek alphabet.";
     hints.style.width = "100%";
     hints.style.position = "absolute";
     document.body.appendChild(hints);
