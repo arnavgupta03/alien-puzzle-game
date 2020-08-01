f2338cd4cd7ba40602ca81ae1310cb48083b022e
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-01 08:00:10 -0400

    Add hint function for Zeta

--- zeta.js
+++ zeta.js
@@ -1,8 +1,9 @@
-function writeHint1() {
-    
-}
-function writeHint2() {
-    
+function writeHints() {
+    var hints = document.createElement("div");
+    hints.innerHTML = "Hint 1: Think about the man stabbed 23 times on March 15. (Et tu, Brute?)  Hint 2: The number you require again relates to what letter Zeta is in the Greek alphabet.";
+    hints.style.width = "100%";
+    hints.style.position = "absolute";
+    document.body.appendChild(hints);
 }
 function checkAnswer(){
 
